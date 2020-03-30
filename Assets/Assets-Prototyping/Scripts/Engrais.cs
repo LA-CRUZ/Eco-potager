52b8b62300eb8c24256174e2d476984ba1566005
lucasgivord <48027211+lucasgivord@users.noreply.github.com>
2020-03-30 14:41:22 +0200

    start to merge code and aading engrais class

---
+++ Assets/Assets-Prototyping/Scripts/Engrais.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Engrais", menuName = "Engrais")]
+public class Engrais : ScriptableObject
+{
+    public string nom = "Engrais riche en ...";
+    public string description = "donner un exemple sur comment faire tel ou tel engrais";
+    public float azote;
+    public float phosphore;
+    public float potassium;
+
+    public Sprite icon;
+}
--- Assets/_Scripts/Plant/PlantScript.cs
+++ Assets/_Scripts/Plant/PlantScript.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Saison
+{
+    Ete,
+    Automne,
+    Hiver,
+    Printemps,
+    None
+}
+
 [CreateAssetMenu(fileName = "New Plant", menuName = "Plant")]
 public class Plant : ScriptableObject
 {
-    public string name;
+    public string nom;
     public string description;
-    public string saison;
-    public Sprite icon;
+    public string pesticide="indiquer ici le pesticide recommendé";
+    public List<Saison> saison;
+    public Nature sol;
 
+    public float tauxHum;
+    public float tauxNut;
+    public float tauxLum;
+
+    public float phMin;
+    public float phMax;
+
+    public float azote;
+    public float phosphore;
+    public float potassium;
+
+    public Sprite icon;
 }
