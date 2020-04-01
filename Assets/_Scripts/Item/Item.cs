e371cd0e8af84116b47190e54360cfeebc8b2a8c
Etienne I <etienne.idoux@etu.univ-lyon1.fr>
2020-04-01 10:58:48 +0200

    arrosage done

--- Assets/_Scripts/Item/Item.cs
+++ Assets/_Scripts/Item/Item.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
 public class Item : ScriptableObject
 {
     public string nom;
