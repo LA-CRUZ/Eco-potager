c6117e6a924b79ceab1c29fcc73d6e8b4351cb32
Hyrastios <hyrastios@hotmail.com>
2020-04-02 13:47:01 +0200

    Implementation of Dialogs

--- Assets/_Scripts/Storage.cs
+++ Assets/_Scripts/Storage.cs
@@ -6,7 +6,7 @@ public class Storage : MonoBehaviour
 {
 
     public GameObject panel;
-    bool state = false;
+    public bool state = false;
 
     // Start is called before the first frame update
     void Start()
